38b786eb9bd1d85e32bb977f200a260f3183d725
George Brata <bratageorge@gmail.com>
2022-08-22 23:27:02 +0300

    added first draft for api docs

--- src/api.js
+++ src/api.js
@@ -1,22 +1,40 @@
 // const express = require("express");
-//  not working ... why? https://stackoverflow.com/questions/41692643/webpack-and-express-critical-dependencies-warning
-
+// not working ... why? https://stackoverflow.com/questions/41692643/webpack-and-express-critical-dependencies-warning
 const express = __non_webpack_require__('express');
+
 const serverless = require("serverless-http");
+// https://github.com/ranm8/requestify
 const requestify = require('requestify'); 
 
 const app = express();
 const router = express.Router();
 
+const BASE = '/.netlify/functions/api';
+
+// router.get("/", (req, res) => {
+//   res.json({
+//     hello: "hi!"
+//   });
+// });
+
 router.get("/", (req, res) => {
+  let docs = `<h1>GET endpoints</h1>` +
+             `<ul>` +
+                `<li><a href="${BASE}/cuvant" target="_blank" />/cuvant</a></li>` +
+                `<li><a href="${BASE}/cuvinte" target="_blank" />/cuvinte</a></li>` +
+              `</ul>`;
+  res.send(docs)
+});
+
+router.get("/cuvant", (req, res) => {
   res.json({
-    hello: "hi!"
+    cuvant: 'cuvant',
+    date: new Date()
   });
 });
 
 
 router.get("/cuvinte", (req, res) => {
-  // let cuvinte = fetch('https://api.sheety.co/06def408e74850aef0fbd22a79539f9f/cuvantulzilei/cuvintePostate').then(res => res.json());
   let cuvinte, postate = [];
   requestify.get('https://api.sheety.co/06def408e74850aef0fbd22a79539f9f/cuvantulzilei/cuvintePostate').then(function(response) {
       cuvinte = response.getBody();
@@ -28,7 +46,7 @@ router.get("/cuvinte", (req, res) => {
   });
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`${BASE}/`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
