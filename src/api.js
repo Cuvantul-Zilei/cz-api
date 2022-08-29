64ac1727e3e612fb2f5b6e4dbb82d3479c03d17c
George Brata <bratageorge@gmail.com>
2022-08-29 22:40:21 +0300

    updated /cuvant endpoint

--- src/api.js
+++ src/api.js
@@ -17,6 +17,22 @@ const BASE = '/.netlify/functions/api';
 //   });
 // });
 
+function humanDate(date) {
+  const formattingOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+  let day = date ? new Date(date) : new Date();
+
+  return day.toLocaleDateString('ro', formattingOptions);
+}
+
+function generateRandom(maxLimit = 100){
+  let rand = Math.random() * maxLimit;
+  console.log(rand); // say 99.81321410836433
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+
 router.get("/", (req, res) => {
   let docs = `<h1>GET endpoints</h1>` +
              `<ul>` +
@@ -28,10 +44,46 @@ router.get("/", (req, res) => {
 });
 
 router.get("/cuvant", (req, res) => {
-  res.json({
-    cuvant: 'cuvant',
-    date: new Date().toLocaleDateString()
+
+  let json = req.query.json === '' || false;
+  let random = req.query.random === '' || false;
+
+  let cuvinte, postate = [];
+  requestify.get('https://api.sheety.co/06def408e74850aef0fbd22a79539f9f/cuvantulzilei/cuvintePostate').then(function(response) {
+      cuvinte = response.getBody();
+      postate = cuvinte.cuvintePostate;
+      
+      if(cuvinte && postate) {
+        let wordIndex;
+        if(random) {
+          wordIndex = generateRandom(postate.length);
+        } else {
+          wordIndex = postate.findIndex(c => humanDate(c.dateCorrect) === humanDate())
+        }
+        if(wordIndex > -1) {
+          let dateFromApi = humanDate(postate[wordIndex].dateCorrect);
+          let cuvantFromApi = postate[wordIndex].cuvant;
+          let definitionFromApi = postate[wordIndex].definition;
+
+            if(json) {
+              res.json({
+                cuvant: cuvantFromApi,
+                definition: definitionFromApi,
+                date: dateFromApi,
+              });
+            } else {
+              res.send(cuvantFromApi)
+            }
+        } else {
+          res.json({
+            notFound: true
+          })
+        }
+        
+      }
   });
+
+  
 });
 
 
