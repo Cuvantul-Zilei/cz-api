814a0c1feb91a7add1a571b172e70f38f31c591d
George Brata <bratageorge@gmail.com>
2022-08-30 17:08:27 +0300

    updated middleware

--- src/api.js
+++ src/api.js
@@ -30,7 +30,7 @@ function generateRandom(maxLimit = 100){
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://cuvantulzilei.ro');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
