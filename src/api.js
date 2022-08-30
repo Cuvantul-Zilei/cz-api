a21e42cd4a024d6e3f4f72bc4cc6ddb4a327b4ee
George Brata <bratageorge@gmail.com>
2022-08-30 17:01:03 +0300

    updated Access-Control-Allow-Origin

--- src/api.js
+++ src/api.js
@@ -30,14 +30,14 @@ function generateRandom(maxLimit = 100){
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cuvantulzilei.ro');
 
   // Request methods you wish to allow
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
