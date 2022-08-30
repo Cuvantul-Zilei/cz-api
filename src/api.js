0581b2c28ff737d8983b922e3951351525d8d792
George Brata <bratageorge@gmail.com>
2022-08-30 17:12:16 +0300

    updated Access-Control-Allow-Headers

--- src/api.js
+++ src/api.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
